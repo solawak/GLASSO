e94265923c5c922607899c3de79db8d231d6a996
solawak <56126461+solawak@users.noreply.github.com>
2024-05-29 16:32:58 +0200

    Update glasso

--- GLASSO.R
+++ GLASSO.R
@@ -23,18 +23,20 @@ lasso <- function(beta, rho, V,u) {
 GLASSO_ <- function(S, rho=1.2, t=0.001, max_iter=100000) {
   p <- ncol(S)
   W = S + rho*diag(1,p,p)
-  Theta = solve(W,diag(1,p,p))
+  Theta = matrix(NA, p,p)
   stop = FALSE
   for (iter in 1:max_iter) {
     for (i in 1:p) {  
-      beta <- -Theta[-i,i]/Theta[i,i] #-Theta12/Theta22
+      beta <- solve(W[-i,-i],diag(1,p-1,p-1))%*%W[-i,i] # W11^-1 * W12
       beta <- lasso(beta, rho, W[-i,-i], S[-i,i])
+      W_lag <- W
+      betas[1:p-1,i] <- beta
+      print(betas)
+      W[-i,i] <- W[-i,-i]%*%beta #W12 = W11*Beta
+      W[i,-i] <- W[-i,-i]%*%beta #W21 = W11*Beta
       Theta[i,i] <- 1/(W[i,i] - W[-i,i]%*%beta) # 1/(W22-W12*Beta)
       Theta[-i,i] <- -Theta[i,i]*beta #Theta12 = -Theta22*Beta
       Theta[i,-i] <- -Theta[i,i]*beta #Theta21 = -Theta22*Beta
-      W_lag <- W
-      W[-i,i] <- W[-i,-i]%*%beta #W12 = W11*Beta
-      W[i,-i] <- W[-i,-i]%*%beta #W21 = W11*Beta
       if (mean((abs(W-W_lag))) < t*(sum(abs(S)) - sum(abs(diag(S))))/(p^2 - p)) {
         stop = TRUE
         print("Break")
@@ -48,6 +50,7 @@ GLASSO_ <- function(S, rho=1.2, t=0.001, max_iter=100000) {
   }
   return(list(Theta, W))
 }
+
 
 X<- GLASSO_(S)
 mdl <- glasso(S,rho=1.2,approx=FALSE, penalize.diagonal = FALSE)
