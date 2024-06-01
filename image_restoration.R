b6f3da04e01e8866ae6dca5ef36e1da660ac4dbc
solawak <56126461+solawak@users.noreply.github.com>
2024-06-01 16:34:52 +0200

    update image_restoration

--- image_restoration.R
+++ image_restoration.R
@@ -25,32 +25,25 @@ get_neighbors <- function(i, j, N, M, d) {
 }
 
 
-calculate_conditional_distribution <- function(i,j,x,y,col_pal,beta,alpha, lambda,sigma, method, d) {
+calculate_conditional_distribution2 <- function(i,j,x,y,col_pal,beta,alpha, lambda,sigma, method, d) {
   neighbors <- get_neighbors(i,j, nrow(x), ncol(x), d)
-  conditional_distribution <- rep(0,length(col_pal))
-  # col_pal <- 0:255 # paleta kolorow
-  #iertujemy po palecie, czyli po kolejnych propozycjach wartości pixela
-  for (k in 1:length(col_pal)) {
-    if (method == "potts") {
-      prior_term <- sum(unlist(lapply(neighbors, function(coord) {
-        return(as.integer(1 - (col_pal[k] == x[coord[1], coord[2]])))# sum(1-I(proposed_x == xij))
-      })))
-    }else {
-      prior_term <- sum(unlist(lapply(neighbors, function(coord) {
-        return(max((lambda*(col_pal[k]-x[coord[1], coord[2]]))^2,alpha))# sum(max((lambda*(proposed_x-xij))^2,alpha))
-      })))
-    }
-    
-    likelihood_term <-  (y[i, j] - col_pal[k] )^2 / (2 * sigma^2)# (y_ij - x_proposed)^2 / (2*sigma^2)
-    conditional_distribution[k] <- exp(-beta * prior_term - likelihood_term)
-  }
-  conditional_distribution = conditional_distribution/sum(conditional_distribution)
+  xs <- unlist(lapply(neighbors, function(coord) {
+    return(x[coord[1], coord[2]])}))
+  prior_term <- sapply(col_pal, function(k) {
+    sum(sapply(xs, function(x) {
+      as.integer(1 - (k == x))
+    }))
+  })
+  likelihood_term <-  (y[i, j] - col_pal)^2 / (2 * sigma^2)# (y_ij - x_proposed)^2 / (2*sigma^2)
+  conditional_distribution <- exp(-beta * prior_term - likelihood_term)
+  conditional_distribution <- conditional_distribution/sum(conditional_distribution)
   return(conditional_distribution)
 }
 
 
 denoise <- function(y, iter_n=1, beta=1,alpha=1, lambda=1,sigma=1, method = "potts", d=1) {
   x <- y
+  all_x <- list()
   paleta <- unique(c(y))
   for (iter in 1:iter_n) {
     T = iter_n/(iter_n+1-iter)
@@ -64,21 +57,21 @@ denoise <- function(y, iter_n=1, beta=1,alpha=1, lambda=1,sigma=1, method = "pot
         new_pixel_value <- sample(paleta, size = 1, prob = conditional_distribution)
         delta_E <- conditional_distribution[which(paleta == x[i,j])] -conditional_distribution[which(paleta == new_pixel_value)]
         # symulowane wyzarzanie
-        if (max(delta*exp(T),1)> runif(1)) {
+        if (max(delta_E*exp(T),1)> runif(1)) {
           x[i,j] <- new_pixel_value
         }
       }
     }
+    # all_x[[iter]]<- x
   }
   return(x)
 }
 
 
 img <- readPNG("lena_noisy.png")
-
-denoised <- denoise(img,beta=1,sigma=1,iter_n=1, d=2)
+denoised <- denoise(img[1:100,1:100],beta=1,sigma=1,iter_n=1, d=2)
 par(mfrow=c(1,2))
-image(t(img)[,ncol(img):1], axes = FALSE, col = grey(seq(0,1,length = 256)))
+image(t(img[1:100,1:100])[,ncol(img[1:100,1:100]):1], axes = FALSE, col = grey(seq(0,1,length = 256)))
 image(t(denoised)[,ncol(denoised):1], axes = FALSE, col = grey(seq(0,1,length = 256)))
 
 #trzeba ustawic dobra lambde i alfe, lamda=1,alpha=1 zwraca szum XD
