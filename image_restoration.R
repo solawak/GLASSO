bf961a3a1c40f3c331daaba5fee849762296980f
solawak <56126461+solawak@users.noreply.github.com>
2024-06-01 03:21:04 +0200

    denoise update

--- image_restoration.R
+++ image_restoration.R
@@ -1,13 +1,15 @@
-install.packages("png")
 library("png")
+library(grid)
+setwd("C:/Users/kasia/OneDrive/Dokumenty/GitHub/GLASSO/")
 
-img <- readPNG("C:/Users/kasia/OneDrive/Dokumenty/GitHub/GLASSO/lena_noisy.png")
-image(t(img)[,ncol(img):1],col = c("black", "white"),axes=FALSE,useRaster=TRUE)
-
-
-get_neighbors <- function(i, j, N, M) {
+get_neighbors <- function(i, j, N, M, d) {
   # Define the potential neighbors
-  potential_neighbors <- list(c(i-1, j), c(i+1, j), c(i, j-1), c(i, j+1))
+  if (d==1) {
+    potential_neighbors <- list(c(i-1, j), c(i+1, j), c(i, j-1), c(i, j+1))
+  }else {
+    potential_neighbors <- list(c(i-1, j), c(i+1, j), c(i, j-1), c(i, j+1), c(i-1,j-1), c(i-1,j+1), c(i+1,j-1), c(i+1,j+1))
+  }
+  
   
   # Filter out neighbors that are outside the matrix
   valid_neighbors <- lapply(potential_neighbors, function(coord) {
@@ -17,22 +19,27 @@ get_neighbors <- function(i, j, N, M) {
       return(NULL)
     }
   })
-  
   # Remove NULL elements
   valid_neighbors <- valid_neighbors[!sapply(valid_neighbors, is.null)]
-  
   return(valid_neighbors)
 }
 
 
-calculate_conditional_distribution <- function(i,j,x,y,beta,sigma) {
-  neighbors <- get_neighbors(i,j, nrow(x), ncol(x))
-  conditional_distribution <- rep(0,2)
-  col_pal <- c(0,1) # paleta kolorow
+calculate_conditional_distribution <- function(i,j,x,y,col_pal,beta,alpha, lambda,sigma, method, d) {
+  neighbors <- get_neighbors(i,j, nrow(x), ncol(x), d)
+  conditional_distribution <- rep(0,length(col_pal))
+  # col_pal <- 0:255 # paleta kolorow
+  #iertujemy po palecie, czyli po kolejnych propozycjach wartości pixela
   for (k in 1:length(col_pal)) {
-    prior_term <- sum(unlist(lapply(neighbors, function(coord) {
-      return(as.integer(1 - (col_pal[k] == x[coord[1], coord[2]])))# sum(1-I(proposed_x == xij))
-    })))
+    if (method == "potts") {
+      prior_term <- sum(unlist(lapply(neighbors, function(coord) {
+        return(as.integer(1 - (col_pal[k] == x[coord[1], coord[2]])))# sum(1-I(proposed_x == xij))
+      })))
+    }else {
+      prior_term <- sum(unlist(lapply(neighbors, function(coord) {
+        return(max((lambda*(col_pal[k]-x[coord[1], coord[2]]))^2,alpha))# sum(max((lambda*(proposed_x-xij))^2,alpha))
+      })))
+    }
     
     likelihood_term <-  (y[i, j] - col_pal[k] )^2 / (2 * sigma^2)# (y_ij - x_proposed)^2 / (2*sigma^2)
     conditional_distribution[k] <- exp(-beta * prior_term - likelihood_term)
@@ -42,24 +49,22 @@ calculate_conditional_distribution <- function(i,j,x,y,beta,sigma) {
 }
 
 
-runif(1)
-denoise <- function(y, beta, sigma, iter_n, method = "potts") {
+denoise <- function(y, iter_n=1, beta=1,alpha=1, lambda=1,sigma=1, method = "potts", d=1) {
   x <- y
+  paleta <- unique(c(y))
   for (iter in 1:iter_n) {
-    T = 1 - (iter-1)/iter_n
+    T = iter_n/(iter_n+1-iter)
     cat(paste0("Iteracja ",iter), "\n")
     for (i in 1:nrow(x)) {
-      if (i%%10==0) {
+      if (i%%50==0) {
         cat(paste0("Iteracja wiersza ",i), "\n")
       }
       for (j in 1:ncol(x)) {
-        conditional_distribution <- calculate_conditional_distribution(i,j,x,y,beta,sigma)
-        new_pixel_value <- sample(0:1, size = 1, prob = conditional_distribution)
-        energy_old <- -log(conditional_distribution[which(0:1 == x[i,j])])
-        energy_new <- -log(conditional_distribution[which(0:1 == new_pixel_value)])
-        delta_E <- energy_new -energy_old
+        conditional_distribution <- calculate_conditional_distribution(i,j,x,y,paleta,beta,alpha,lambda,sigma,method, d)
+        new_pixel_value <- sample(paleta, size = 1, prob = conditional_distribution)
+        delta_E <- conditional_distribution[which(paleta == x[i,j])] -conditional_distribution[which(paleta == new_pixel_value)]
         # symulowane wyzarzanie
-        if (max(exp(-delta_E/T),1)> runif(1)) {
+        if (max(delta*exp(T),1)> runif(1)) {
           x[i,j] <- new_pixel_value
         }
       }
@@ -68,8 +73,22 @@ denoise <- function(y, beta, sigma, iter_n, method = "potts") {
   return(x)
 }
 
-denoised <- denoise(img,1,1,2)
+
+img <- readPNG("lena_noisy.png")
+
+denoised <- denoise(img,beta=1,sigma=1,iter_n=1, d=2)
 par(mfrow=c(1,2))
-image(t(img)[,ncol(img):1],col = c("black", "white"),axes=FALSE,useRaster=TRUE)
-image(t(denoised)[,ncol(img):1],col = c("black", "white"),axes=FALSE,useRaster=TRUE)
+image(t(img)[,ncol(img):1], axes = FALSE, col = grey(seq(0,1,length = 256)))
+image(t(denoised)[,ncol(denoised):1], axes = FALSE, col = grey(seq(0,1,length = 256)))
+
+#trzeba ustawic dobra lambde i alfe, lamda=1,alpha=1 zwraca szum XD
+denoised2 <- denoise(img,alpha=2,lambda=5,sigma=1,iter_n=10, d=1, method="2")
+par(mfrow=c(1,2))
+image(t(img)[,ncol(img):1], axes = FALSE, col = grey(seq(0,1,length = 256)))
+image(t(denoised2)[,ncol(denoised2):1], axes = FALSE, col = grey(seq(0,1,length = 256)))
+
+png(filename = "denoised2.png", width = nrow(denoised2), height = ncol(denoised2))
+grid.raster(denoised2, interpolate=FALSE)
+dev.off()
+
 
